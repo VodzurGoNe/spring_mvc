42014271b608fb72d8962da3830b03057392c126
Vladislav Gruzdov <Gruzdov.V.1991@Hotmail.com>
2021-05-16 10:56:05 +0400

    Освоил анатоцию @RequestParam

--- spring_course_mvc/src/main/java/org/vladislavgruzdov/spring/mvc/MyController.java
+++ spring_course_mvc/src/main/java/org/vladislavgruzdov/spring/mvc/MyController.java
@@ -3,10 +3,12 @@ package org.vladislavgruzdov.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -24,6 +26,16 @@ public class MyController {
         return "show-emp-details-view";
     }
 */
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@RequestParam("employeeName") String empName
+            , Model model) {
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+
+        return "show-emp-details-view";
+    }
+/*
     @RequestMapping("/showDetails")
     public String showEmpDetails(HttpServletRequest request, Model model) {
         String empName = request.getParameter("employeeName");
@@ -32,4 +44,5 @@ public class MyController {
 
         return "show-emp-details-view";
     }
+ */
 }
