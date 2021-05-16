6da89f84ded4c399ea0f1bf053a32b49460d545a
Vladislav Gruzdov <gruzdov.v.1991@hotmail.com>
2021-05-16 12:19:32 +0400

    Spring MVC форма select

--- spring_course_mvc/src/main/java/org/vladislavgruzdov/spring/mvc/Employee.java
+++ spring_course_mvc/src/main/java/org/vladislavgruzdov/spring/mvc/Employee.java
@@ -5,6 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -14,4 +17,13 @@ public class Employee {
     private String surname;
     private Integer salary;
     private String department;
+    @ToString.Exclude
+    private Map<String, String> departments;
+
+    {
+        departments = new HashMap<>();
+        departments.put("Information Technology", "IT");
+        departments.put("Human Resources", "HR");
+        departments.put("Sales","Sales");
+    }
 }
